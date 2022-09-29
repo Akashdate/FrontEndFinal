ae290cb20b9bf46ce8e0dd652f70c363226cbe6a
Akashdate <akashdate786@gmail.com>
2022-09-29 23:45:25 +0530

    full n final

--- HSP_REACT_FINAL/src/boot-api.js
+++ HSP_REACT_FINAL/src/boot-api.js
@@ -1,4 +1,4 @@
-const base_url = "http://localhost:8080";
-//const base_url = "http://3.111.151.63:8080"; /*---> for deployed server on cloud*/
+// const base_url = "http://localhost:8080";
+const base_url = "http://3.111.151.63:8080"; /*---> for deployed server on cloud*/
 
 export default base_url;
